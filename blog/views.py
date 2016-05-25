2249422376653fff336edd8b2a5b4ad2f17c8e05
l1218yj2 <l1218yj2@github.com>
2016-05-25 05:41:57 +0000

    add index and detail function

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def index(request):
+    return render(request, 'index.html', context ={
+        blog : Blog.objects.all()
+    })
+
+def detail(reqeust, pk= None):
+    return render(request, 'detail.html', context = {
+
+    })
