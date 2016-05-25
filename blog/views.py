1c73e652ff1def632bab4f4c53967f5f67fc3231
l1218yj2 <l1218yj2@github.com>
2016-05-25 05:51:07 +0000

    some error fix and migrations

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import *
 # Create your views here.
 def index(request):
     return render(request, 'index.html', context ={
-        blog : Blog.objects.all()
+        'blog' : Blog.objects.all()
     })
 
 def detail(reqeust, pk= None):
--- lecture3/settings.py
+++ lecture3/settings.py
@@ -126,4 +126,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
